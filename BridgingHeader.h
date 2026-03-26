9cb4dff40d2406c2344e649d62f1dd830419ed8c
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-03-26 17:21:08 +0800

    motor + drivers

--- BridgingHeader.h
+++ BridgingHeader.h
@@ -4,25 +4,22 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/pwm.h"
-#include "pico/multicore.h"
-#include "tusb.h"
 #include "PWMHelpers.h"
+
+void multicore_launch_core1(void (*entry)(void));
 
 static inline void usb_wait_core()
 {
     stdio_init_all();
-    while (!tud_cdc_connected())
-    {
-        sleep_ms(10);
-    }
+    sleep_ms(2000);
 }
 
-static inline void print_init()
+static inline void launch_usb_core()
 {
     multicore_launch_core1(usb_wait_core);
 }
 
 static inline void print(const char *s)
 {
-    printf("%s", s);
+    printf("%s\n", s);
 }
--- Main.swift
+++ Main.swift
@@ -20,44 +20,164 @@ struct Main {
         }
     }
 
-    struct Pin {
-        var state: pinState = .low
-        var name: String
+    enum MotorSelection {
+        case motor1
+        case motor2
     }
 
+    struct Pin {
+        let number: UInt32
+        var state: pinState = .high
+
+         init(number: UInt32) {
+            self.number = number
+            gpio_init(number)
+            gpio_set_dir(number, false) // starting state
+        }
+
+        mutating func on() {
+            state.on()
+            gpio_put(number, true)
+        }
+
+        mutating func off() {
+            state.off()
+            gpio_put(number, false)
+        }
+
+        mutating func toggle() {
+            state.toggle()
+            gpio_put(number, state == .high)
+        }
 
 
+    }
 
+    struct PWMPin {
+        let slice: UInt32
+        let channel: UInt32
+        var level: Int32 = 0
+
+        init(pin: UInt32) {
+            gpio_set_function(pin, GPIO_FUNC_PWM)
+            slice = pwm_slice_for_gpio(pin)
+            channel = pwm_channel_for_gpio(pin)
+            var config = pwm_get_default_config()
+            pwm_init(slice, &config, true)
+            pwm_set_wrap(slice, 255)
+        }
+
+        mutating func setLevel(_ newLevel: Int32) {
+            level = max(0, min(255, newLevel))
+            pwm_set_chan_level(slice, channel, UInt16(level))
+        }
+    }
+
+    struct Motor {
+        var pin1: Pin
+        var pin2: Pin
+    }
+
+    struct MotorDriver {
+        var pwmPin: PWMPin
+        var motor1: Motor
+        var motor2: Motor
+
+        init(pwmPinNumber: UInt32, motor1: Motor, motor2: Motor) {
+            pwmPin = PWMPin(pin: pwmPinNumber)
+            self.motor1 = motor1
+            self.motor2 = motor2
+        }
+
+        mutating func setMotor1Direction(forward: Bool) {
+            if forward {
+                motor1.pin1.on()
+                motor1.pin2.off()
+            } else {
+                motor1.pin1.off()
+                motor1.pin2.on()
+            }
+        }
+
+        mutating func setMotor2Direction(forward: Bool) {
+            if forward {
+                motor2.pin1.on()
+                motor2.pin2.off()
+            } else {
+                motor2.pin1.off()
+                motor2.pin2.on()
+            }
+        }
+
+        mutating func setMotorSpeed(_ speed: Float) {
+            let scaledSpeed = Int32(max(0.0, min(1.0, speed)) * 255.0)
+            pwmPin.setLevel(scaledSpeed)
+        }
+
+        mutating func turnMotor1(on: Bool) {
+            if on {
+                motor1.pin1.on()
+                motor1.pin2.off()
+            } else {
+                motor1.pin1.off()
+                motor1.pin2.off()
+            }
+        }
+
+        mutating func turnMotor2(on: Bool) {
+            if on {
+                motor2.pin1.on()
+                motor2.pin2.off()
+            } else {
+                motor2.pin1.off()
+                motor2.pin2.off()
+            }
+        }
+
+        mutating func enableMotor(_ motor: MotorSelection, for duration: Float) {
+            let durationMs = Int32(max(0.0, duration) * 1000.0)
+
+            switch motor {
+            case .motor1:
+                turnMotor1(on: true)
+            case .motor2:
+                turnMotor2(on: true)
+            }
+
+            let callback: @convention(c) (Int32, UnsafeMutableRawPointer?) -> Int64 = { id, userData in
+                let driver = userData!.assumingMemoryBound(to: MotorDriver.self)
+                switch motor {
+                case .motor1:
+                    driver.pointee.turnMotor1(on: false)
+                case .motor2:
+                    driver.pointee.turnMotor2(on: false)
+                }
+                return 0
+            }
+
+            add_alarm_in_ms(durationMs, callback, &self, false)
+        }
+    }
 
 
 
     static func main() {
     
-        multicore_launch_core1(usb_wait_core)
+        // debug print init
+        launch_usb_core()
 
+        // status LED
+        var statusPWM = PWMPin(pin: UInt32(PICO_DEFAULT_LED_PIN))
+        var statusDirection: Int32 = 10
 
-        let pin = UInt32(PICO_DEFAULT_LED_PIN)
-    
-        gpio_set_function(pin, GPIO_FUNC_PWM)
-        let slice = pwm_slice_for_gpio(pin)
-        let channel = pwm_channel_for_gpio(pin)
-    
-        var config = pwm_get_default_config()
-        pwm_init(slice, &config, true)
-        pwm_set_wrap(slice, 255)
-    
-        var level: Int32 = 0
-        var direction: Int32 = 10
-    
-        sleep_ms(2000)
-        print("Starting PWM...\n")
-    
+        // motor driver
+        
+
         while true {
-            pwm_set_chan_level(slice, channel, UInt16(level))
-            level += direction
-            if level >= 255 { direction = -1 }
-            if level <= 0   { direction = 1 }
+            statusPWM.setLevel(statusPWM.level + direction)
+            if statusPWM.level >= 255 { direction = -1 }
+            if statusPWM.level <= 0   { direction = 1 }
             sleep_ms(6)
         }
-  }
+    }
 }
